69945c762b62a4592f7807f5a4577ac8231d49b6
Azan <itsazanwaseem@gmail.com>
2025-12-09 21:59:49 +0500

    Add A+ and D+ grades to grade options with correct point values

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ const DEPARTMENTS = {
 };
 
 const GRADE_POINTS = {
-  'A': 4.00, 'A-': 3.67,
+  'A+': 4.00, 'A': 4.00, 'A-': 3.67,
   'B+': 3.33, 'B': 3.00, 'B-': 2.67,
   'C+': 2.33, 'C': 2.00, 'C-': 1.67,
-  'D': 1.00, 'F': 0.00
+  'D+': 1.33, 'D': 1.00, 'F': 0.00
 };
 
 const GRADE_OPTIONS = Object.keys(GRADE_POINTS);
